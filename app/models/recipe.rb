7771b178e0decd9a215ca748577bec6912220728
Shawn <email": null>
2016-07-13 12:05:58 -0400

    fixe missing comment in category migration and add presence validations for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,5 @@ class Recipe < ActiveRecord::Base
   has_many   :amount_of_ingredients, through: :amount_of_ingredients, source: :ingredients
 
   validates :employee_id, uniqueness: { scope: :area_id }
+  validates :user, :category, :amount_of_ingredients, presence: true
 end
