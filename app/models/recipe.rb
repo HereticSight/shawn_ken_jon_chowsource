b5970f99eeb81fef38cee25a258b28ad59abc022
Shawn <email": null>
2016-07-13 11:59:50 -0400

    add migrations for recipes and categorys

---
+++ app/models/recipe.rb
@@ -0,0 +1,8 @@
+class Recipe < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :category
+  has_many   :amount_of_ingredients
+  has_many   :amount_of_ingredients, through: :amount_of_ingredients, source: :ingredients
+
+  validates :employee_id, uniqueness: { scope: :area_id }
+end
---
+++ db/migrate/20160713152926_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table   :recipes do |t|
+      t.string     :name, limit: 120
+      t.integer    :difficulty, null: false
+      t.text       :description, null: false
+      t.text       :instructions, null: false
+      t.references :user, null: false
+      t.references :category_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
