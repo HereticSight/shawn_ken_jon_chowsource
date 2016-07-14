cfc3f25ea3b3f9ebb7832bfbcd2c2dd444806f1f
Kenny Phu <email": null>
2016-07-14 10:38:12 -0400

    update routes to include new, update logic to determine if a user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :logged_in?
+  before_action :current_user
 
   def hello
     render html: "Hello, World!"
@@ -16,9 +16,5 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def logged_in?
-    !current_user.nil?
   end
 end
