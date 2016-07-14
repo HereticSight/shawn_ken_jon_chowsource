7cc41069a6f63c6da36c174468f56333d7a54962
Kenny Phu <email": null>
2016-07-14 17:12:44 -0400

    add pagination to user and recipe show views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 	def show
 		@category = Category.find(params[:id])
+    @recipes = Recipe.where(category:@category).paginate(page: params[:page], per_page: 5)
 		render 'categories/show'
 	end
 end
