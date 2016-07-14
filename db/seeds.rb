0a78c107e03a4d615b8b315ba3daaf8ce3d1c4eb
Conturbo <Conturbodev@gmail.com>
2016-07-14 11:17:43 -0400

    Seeded Grandma

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-recipes = Recipes.create([{}])
+# Basic Users
+User.create({
+	name: "Grandma",
+	email: "OriginalGrandma@OGma.com",
+	password: "grandma"
+	})
 
 
 # Basic Categories
@@ -20,6 +24,46 @@ Category.create({ name: "Beverages"})
 
 # Basic Recipes
 Recipe.create({
-	name: "Soup", difficulty: nil, description: nil, instructions: nil, user_id: nil, category_id
+	name: "Soup", 
+	difficulty: 1, 
+	description: "Great soup", 
+	instructions: "Bop it, stop it, grab it, Pop it.",
+	user_id: 1, 
+	category_id: 1
 	})
 
+Recipe.create({
+	name: "Soup", 
+	difficulty: 1, 
+	description: "Great soup", 
+	instructions: "Bop it, stop it, grab it, Pop it.",
+	user_id: 1, 
+	category_id: 2
+	})
+
+Recipe.create({
+	name: "Chocolate", 
+	difficulty: 1, 
+	description: "Great soup", 
+	instructions: "Bop it, stop it, grab it, Pop it.",
+	user_id: 1, 
+	category_id: 3
+	})
+
+Recipe.create({
+	name: "Raspberries", 
+	difficulty: 1, 
+	description: "Great soup", 
+	instructions: "Bop it, stop it, grab it, Pop it.",
+	user_id: 1, 
+	category_id: 4
+	})
+
+Recipe.create({
+	name: "Sandwich", 
+	difficulty: 1, 
+	description: "Great soup", 
+	instructions: "Bop it, stop it, grab it, Pop it.",
+	user_id: 1, 
+	category_id: 5
+	})
