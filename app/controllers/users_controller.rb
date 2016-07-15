e70e89845fbc1d297dc8ec59d65f02c03e3cae0d
Kenny Phu <email": null>
2016-07-15 10:38:11 -0400

    increase the pagination for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 5)
+    @users = User.paginate(page: params[:page], per_page: 30)
   end
 
   def new
