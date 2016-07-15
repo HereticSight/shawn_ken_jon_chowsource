cb7218f8354c60b4e754fb0b200e35e249f34783
Shawn <email": null>
2016-07-15 14:42:08 -0400

    tweak form for ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,16 +2,20 @@ class RatingsController < ApplicationController
 	before_action :redirect_anon
   def create
   	@recipe = Recipe.find(params[:recipe_id])
-  	@rating = Rating.new( value: params[:value], user: current_user, recipe: @recipe) 
+  	@rating = Rating.new( value: params[:value], user: current_user, recipe: @recipe)
   	if @rating.save
   		flash[:success] = "You have successfully rated the recipe!"
   		redirect_to @recipe
   	else
-  		flash[:danger] = "Failed to rate the recipe." 
-  		redirect_to @recipe 
+  		flash[:danger] = "Failed to rate the recipe."
+  		redirect_to @recipe
   	end
   end
 
-  def edit
+  def update
+    # binding.pry
+    rating = Rating.find(params[:rating][:id])
+    rating.update(params[:rating])
+    redirect_to recipe_path(params[:recipe_id])
   end
 end
