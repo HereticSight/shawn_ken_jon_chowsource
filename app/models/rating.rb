a289eee3879893fe8087fe5bac0ab79b5b69fa22
Kenny Phu <email": null>
2016-07-15 16:36:54 -0400

    update rating model to ensure that users can only vote on each recipe once

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,5 +2,5 @@ class Rating < ActiveRecord::Base
   belongs_to :rater, class_name: 'User', foreign_key: "user_id"
   belongs_to :recipe
 
-  validates :recipe, uniqueness: { scope: :rater }
+  validates :rater, uniqueness: { scope: :recipe }
 end
